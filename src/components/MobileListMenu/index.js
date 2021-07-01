af6eae6dd6476c06aec7c4a6f2eec86cd5691a40
tomruffhightech <tomruffhightech@gmail.com>
2021-07-01 10:35:16 -0600

    on the right track to complete the mobile side list bar

--- src/components/MobileListMenu/index.js
+++ src/components/MobileListMenu/index.js
@@ -19,41 +19,39 @@ export default function MobileListMenu({
 }) {
   const [menuOpen, setMenuOpen] = useState(false);
   const [menuClass, setMenuClass] = useState('');
-  const menuChange = (e)=>{
-    if(e){
+  const menuChange = (e) => {
+    if (e) {
       setMenuOpen(true)
-    }  
-    if (menuOpen){
+    }
+    if (menuOpen) {
       setMenuOpen(false)
     }
   }
-  const menuStyles = {
-    one:{
-      transform:'rotate(45deg)',
-      width:"30px",
-      margin:'auto'
-    },
-    two:{
-      display:'none',
-    },
-    three:{
-      transform:'rotate(-45deg)',
-      width:"30px",
-      margin:'auto'
-    }
-  }
+
   return (
     <div className="m-menu-list">
+      {/* need to make this a sepparate component */}
+      {/* then move to main taskapp component */}
       <div className='menu-icon' onClick={e => menuChange(e)}>
-        <div className='icon-cont'>
-          <div id='m-i-one' className='m-icon-item ' style={menuOpen? menuStyles.one:{display:"block"} }></div>
-          <div id='m-i-two' className='m-icon-item' style={menuOpen? menuStyles.two:{display:"block"} }></div>
-          <div id='m-i-three' className='m-icon-item' style={menuOpen? menuStyles.three:{display:"block"} }></div>
+        <div className={menuOpen ? "icon-cont-open" : 'icon-cont'} >
+          <div className={menuOpen ? "m-icon-o-one m-icon-item" : 'm-icon-item m-ii-one '} ></div>
+          <div className={menuOpen ? "m-icon-o-two m-icon-item" : 'm-icon-item m-ii-two'} ></div>
+          <div className={menuOpen ? "m-icon-o-three m-icon-item" : 'm-icon-item m-ii-three'} ></div>
         </div>
       </div>
-
-      <div className={menuOpen? "menu-items-cont": "menu-closed"} >
-
+      {/* need to make this a sepparate component  */}
+      {/* then move to main sidebar component since logic is already there for list names*/}
+      <div className={menuOpen ? "menu-items-cont" : "menu-closed"} >
+        <h4>Menu Items</h4>
+        <div className="menu-list-name">
+          List name
+        </div>
+        <div className="menu-list-name">
+          List name
+        </div>
+        <div className="menu-list-name">
+          List name
+        </div>
       </div>
     </div>
   )
