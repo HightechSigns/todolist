8c1ea4ec2355109eb8642ff429c30490c020ad86
Thomas Ruff <thomasruff@thomass-air.lan>
2021-07-09 16:46:49 -0600

    mobile menu is working as intended everything functions great. time to finetune

--- src/components/ListsSideBar/index.js
+++ src/components/ListsSideBar/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./style.css";
+// import assets
+import addNewListImg from "../../assets/images/addnewList.png"
 import addLight from "../../assets/images/addLight.svg";
 import addDark from "../../assets/images/addDark.svg";
 import trashDark from "../../assets/images/trashDark.svg";
@@ -24,11 +26,12 @@ export default function ListsSideBar({
   addTask,
   setCurrentListName,
   menuOpen,
-  setMenuOpen
+  setMenuOpen,
 }) {
   const listNameInput = useRef();
   const [add, setAdd] = useState(false);
-  const [hover, setHover] = useState('');
+  const [mbAdd, setMbAdd] = useState(false);
+  const [hover, setHover] = useState("");
   const [listNameVal, setListNameVal] = useState("");
   const actID = useSelector((state) => state.actID);
   const toggle = useSelector((state) => state.toggle);
@@ -42,8 +45,8 @@ export default function ListsSideBar({
 
   // handle the delete of the list
   const handleDeleteList = (e, id) => {
-    console.log("delete id = " + id)
-    console.log("active id = " + actID)
+    console.log("delete id = " + id);
+    console.log("active id = " + actID);
 
     if (e) {
       setListDelete(true);
@@ -51,16 +54,15 @@ export default function ListsSideBar({
         if (d.id === id) {
           data.splice(i, 1);
           console.log("deleted List: " + id);
-
         }
       });
       if (data.length >= 1) {
         let newCurrId = data[0].id;
-        setCurrentListName(data[0].name)
+        setCurrentListName(data[0].name);
         setLocalActiveId(newCurrId);
         dispatch(setActiveId(""));
         dispatch(setActiveId(newCurrId));
-        console.log("new current id apparently = " + newCurrId)
+        console.log("new current id apparently = " + newCurrId);
       } else {
         console.log("No more lists");
         setLocalActiveId("");
@@ -81,37 +83,32 @@ export default function ListsSideBar({
     dispatch(setActiveId(obj.id));
     // set the active list id in Local storage
     setLocalActiveId(obj.id);
-    setCurrentListName(obj.name)
+    setCurrentListName(obj.name);
     // -----------
     setAdd(false);
     setlocalLoaded(true);
-  };
-  // handle the click for adding a name. pops up the modal
-  const handleAddClick = (e) => {
-    if (e && !add) {
-      setAdd(true);
-    } else {
-      setAdd(false);
-    }
+    setMenuOpen(false);
+    setListNameVal('');
+    setMbAdd(false);
   };
   // if user clicks off the iput area for the list, close it
   const clickOff = (e) => {
     if (listNameInput.current.contains(e.target)) {
-      return
+      return;
     }
-    setAdd(false)
-  }
+    setAdd(false);
+  };
   // handles the active list shows little border on the right
   const handleActiveList = (id) => {
     if (id !== actID) {
       dispatch(setActiveId(""));
       dispatch(setActiveId(id));
       setLocalActiveId(id);
-      data.map(d => {
+      data.map((d) => {
         if (d.id === id) {
-          setCurrentListName(d.name)
+          setCurrentListName(d.name);
         }
-      })
+      });
     } else {
       console.log("List ID is alreaddy active");
     }
@@ -131,7 +128,6 @@ export default function ListsSideBar({
             type="text"
             placeholder="List Name"
             autoFocus
-
           />
         </form>
         <div
@@ -143,13 +139,12 @@ export default function ListsSideBar({
   useEffect(() => {
     console.log("active Id has changed and component has reloaded");
     //click off the input area for adding list name
-    document.addEventListener('mousedown', clickOff);
+    document.addEventListener("mousedown", clickOff);
     setListDelete(false);
     // need to check local storage to see if anything has changed
     // if changed then post new data to local storage
     updateLocalData(data);
   }, [actID, listDelete]);
-
 
   return (
     <div>
@@ -161,7 +156,7 @@ export default function ListsSideBar({
           </p>
           <img
             ref={listNameInput}
-            onClick={(e) => handleAddClick(e)}
+            onClick={(e) => (add ? setAdd(false) : setAdd(true))}
             src={toggle ? addLight : addDark}
             alt="#"
             style={{ cursor: "pointer" }}
@@ -173,97 +168,131 @@ export default function ListsSideBar({
           {/* this will be mapped when data gets loaded */}
           {localLoaded && data.length >= 1
             ? data.map((d, i) => (
-              <div key={i} className="list-name-outer-cont">
-                <ProgressNote
-                  data={d}
-                  taskSuccess={taskSuccess}
-                  taskDelete={taskDelete}
-                  addTask={addTask}
-                />
-                <div
-
-                  data-tagid={d.id}
-                  className="list-name"
-                  onClick={(e) => handleActiveList(d.id)}
-                  onMouseOver={(e) => setHover(d.id)}
-                  onMouseLeave={(e) => setHover('')}
-                >
-                  {localLoaded && actID === d.id ? (
-                    <div
-                      className="list-active-bar"
-                      style={
-                        toggle
-                          ? { background: "#20FC8F" }
-                          : { background: "#2e4756" }
-                      }
-                    ></div>
-                  ) : (
-                    ""
-                  )}
-                  <p style={{ textTransform: "capitalize" }}>{d.name}</p>
-                  {hover === d.id ? (
-                    <img
-                      src={trashDark}
-                      alt="#"
-                      onClick={(e) => handleDeleteList(e, d.id)}
-                      style={{ cursor: "pointer", width: "15px" }}
-                    />
-                  ) : (
-                    ""
-                  )}
-
+                <div key={i} className="list-name-outer-cont">
+                  <ProgressNote
+                    data={d}
+                    taskSuccess={taskSuccess}
+                    taskDelete={taskDelete}
+                    addTask={addTask}
+                  />
+                  <div
+                    data-tagid={d.id}
+                    className="list-name"
+                    onClick={(e) => handleActiveList(d.id)}
+                    onMouseOver={(e) => setHover(d.id)}
+                    onMouseLeave={(e) => setHover("")}
+                  >
+                    {localLoaded && actID === d.id ? (
+                      <div
+                        className="list-active-bar"
+                        style={
+                          toggle
+                            ? { background: "#20FC8F" }
+                            : { background: "#2e4756" }
+                        }
+                      ></div>
+                    ) : (
+                      ""
+                    )}
+                    <p style={{ textTransform: "capitalize" }}>{d.name}</p>
+                    {hover === d.id ? (
+                      <img
+                        src={trashDark}
+                        alt="#"
+                        onClick={(e) => handleDeleteList(e, d.id)}
+                        style={{ cursor: "pointer", width: "15px" }}
+                      />
+                    ) : (
+                      ""
+                    )}
+                  </div>
                 </div>
-              </div>
-
-            ))
+              ))
             : ""}
           {/* ---------------------------------------- */}
         </div>
       </div>
       {/* ------------------ Mobile menu ------------------- */}
-      <div className={menuOpen ? 'sbl-mb-overlay-open' : 'sbl-mb-overlay-closed'} onClick={e => setMenuOpen(false)}>
+      <div
+        className={menuOpen ? "sbl-mb-overlay-open" : "sbl-mb-overlay-closed"}
+        onClick={(e) => setMenuOpen(false)}
+      >
         {/* empty */}
       </div>
-      <div className={menuOpen ? 'sbl-mb-cont-open' : 'sbl-mb-cont-closed'}>
+      <div className={menuOpen ? "sbl-mb-cont-open" : "sbl-mb-cont-closed"}>
         {/* ------------- header for menu ------------- */}
-        <div className="sbl-mb-header" style={menuOpen ? { display: 'flex' } : { display: 'none' }}>
+        <div
+          className="sbl-mb-header"
+          style={menuOpen ? { display: "flex" } : { display: "none" }}
+        >
           <div className="sbl-mb-h-add">
-            <p style={{ color: "#ffffff50" }}>
-              Lists
-            </p>
+            <p style={{ color: "#ffffff50" }}>Lists</p>
             <img
-              ref={listNameInput}
-              onClick={(e) => handleAddClick(e)}
+              onClick={(e) => (mbAdd ? setMbAdd(false) : setMbAdd(true))}
               src={addLight}
               alt="#"
-              style={{ cursor: "pointer" }}
+              className={mbAdd ? "mb-add-img-true" : "mb-add-img-false"}
               role="button"
               aria-label="Create a new list..."
             />
           </div>
-          <div className='sbl-input-add-cont'>
-            <input type="text" placeholder='New List' className="sbl-input" />
-            <button className="mobile-add-btn">Add</button>
-          </div>
+          {mbAdd ? (
+            <div className="sbl-input-add-cont">
+              <input
+                type="text"
+                placeholder="New List"
+                className="sbl-input"
+                spellcheck="true"
+                onChange={(e) => handleChange(e)}
+                type="text"
+                placeholder="List Name"
+                autoFocus
+                value={listNameVal}
+              />
+              <button className="mobile-add-btn" onClick={(e) => handleSubmit(e)}>Add</button>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
         {/* ---------- Lists -------------- */}
-        <div className="sbl-mb-list-cont" style={menuOpen ? { display: 'flex' } : { display: 'none' }}>
-          <div className="sbl-list-item" style={{ background: "#243641" }}>
-            <div className='sbl-active' style={{ background: "#20FC8F" }}>
-            </div>
-            <div className="mb-li-selector">
-              <p style={{ color: 'white' }}>List Name</p>
-            </div>
-            <div className="mb-li-delete-btn">
-              <img
-                src={trashLight}
-                alt="#"
-              />
-            </div>
-          </div>
+        <div
+          className="sbl-mb-list-cont"
+          style={menuOpen ? { display: "flex" } : { display: "none" }}
+        >
+          {menuOpen && data.length >= 1
+            ? data.map((d, i) => (
+                <div
+                  className="sbl-list-item"
+                  style={{ background: "#243641" }}
+                  key={i}
+                >
+                  <div
+                    className="sbl-active"
+                    style={
+                      actID === d.id
+                        ? { background: "#20FC8F" }
+                        : { background: "none" }
+                    }
+                  ></div>
+                  <div
+                    className="mb-li-selector"
+                    onClick={(e) => handleActiveList(d.id)}
+                  >
+                    <p style={{ color: "white" }}>{d.name}</p>
+                  </div>
+                  <div
+                    className="mb-li-delete-btn"
+                    onClick={(e) => handleDeleteList(e, d.id)}
+                  >
+                    <img src={trashLight} alt="#" />
+                  </div>
+                </div>
+              ))
+            : ''}
+            {data.length === 0 && !mbAdd?<img className="mb-add-list-img" src={addNewListImg} alt="add list" />:""}
         </div>
       </div>
     </div>
-
   );
 }
--- src/pages/TodoList/index.js
+++ src/pages/TodoList/index.js
@@ -90,6 +90,6 @@ export default function TodoList() {
           getName={getName}
         />
       </section>
-    </div>
+    </div> 
   );
 }
