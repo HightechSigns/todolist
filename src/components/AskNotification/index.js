bc5e976d205f1a4cfe8e817e326db8b278b8e3eb
Thomas Ruff <thomasruff@Thomass-MacBook-Air.local>
2021-08-30 09:47:31 -0600

    adding new files and folders to this branch

---
+++ src/components/AskNotification/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AskNotification({btnFunc, title, message,effect}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/TaskOptions/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TaskOptions({id,}) {
+    // options are, edit task, add desc, 
+    return (
+        <div>
+            
+        </div>
+    )
+}
